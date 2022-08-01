2f200acd6a23dee8194f7bf49387df4078fbf486
Owainxd <mermozrpz@gmail.com>
2022-08-01 12:13:33 +0200

    first commit

---
+++ src/main/java/fr/mybank/TestDevApplication.java
@@ -0,0 +1,13 @@
+package fr.mybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDevApplication.class, args);
+	}
+
+}

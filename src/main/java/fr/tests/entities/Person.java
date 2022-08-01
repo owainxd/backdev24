f8041a094b45f902a589a3245c7329aa898283fa
Owainxd <mermozrpz@gmail.com>
2022-08-01 12:33:27 +0200

    class person changed

--- src/main/java/fr/tests/entities/Person.java
+++ src/main/java/fr/tests/entities/Person.java
@@ -5,30 +5,13 @@ public class Person {
 	private Long id; 
 	private String nom; 
 	private String ville;
+	
 	public Person(Long id, String nom, String ville) {
 		super();
 		this.id = id;
 		this.nom = nom;
 		this.ville = ville;
 	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public String getVille() {
-		return ville;
-	}
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
-
+	
 
 }
